a68f53aa04f8a4ef0205da4816ac7f56466a830a
Aleksandr Maksimov <alexandmaximov@gmail.com>
2022-03-23 11:21:32 +0300

    Сделал 3-е задание

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/questions', to: 'questions#index' # localhost/question HTTP GET
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root 'pages#index'
+  resources :questions # если нужны не все методы то пишем , only: %i[index new edit create update destroy show]
 
+  root 'pages#index'
 end
