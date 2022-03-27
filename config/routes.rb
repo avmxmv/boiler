d299179fa03118c93a9409ea8dba5811c474e2f8
Aleksandr Maksimov <alexandmaximov@gmail.com>
2022-03-27 18:40:00 +0300

    Прошёл 5-ый урок

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :questions # если нужны не все методы то пишем , only: %i[index new edit create update destroy show]
+  # если нужны не все методы то пишем , only: %i[index new edit create update destroy show]
+  resources :questions do
+    resources :answers, only: %i[create destroy]
+  end
 
   root 'pages#index'
 end
